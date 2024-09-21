c5be2686f5a452254e9040e7058b54538edd2195
great-special <sirgreatspecial@gmail.com>
2024-09-21 10:36:17 +0100

    production five added Cloudinary

--- decovista_end/settings.py
+++ decovista_end/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'djoser',
+    'cloudinary',
+    'cloudinary_storage',
     
     # Our apps
     'appointment.apps.AppointmentConfig',
@@ -118,6 +120,15 @@ DATABASES = {
     }
 }
 
+#  Media Files
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('API_KEY'),
+    'API_SECRET': env('API_SECRET'),
+}
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Password validation
