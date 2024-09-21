3263585b646f7391743d73000f0c8d2a70225e86
great-special <sirgreatspecial@gmail.com>
2024-09-21 01:00:11 +0100

    blog app logic

--- appointment/urls.py
+++ appointment/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from appointment import views
 
 urlpatterns = [
-    path('', views.ConsultationView.as_view()),
-    path('<int:pk>/', views.ConsultationDetail.as_view()),
+    path('', views.ConsultationView.as_view(), name='consultation-list-create'),
+    path('<int:pk>/', views.ConsultationDetail.as_view(), name='consultation-detail'),
 ]
 
