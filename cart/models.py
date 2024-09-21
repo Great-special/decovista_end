8f9d15745e1bfc178e69e1fd8f98b71a48595812
RamzypeeTech <okerekeprince111@gmail.com>
2024-09-21 01:52:04 +0100

    eight commit

--- cart/models.py
+++ cart/models.py
@@ -1,2 +1,29 @@
 from django.db import models
 from product.models import Products
+
+from django.db import models
+from product.models import Products
+    
+
+class Cart(models.Model):
+    products = models.ManyToManyField(Products, through='CartItem')
+
+    def total_amount(self):
+        total = sum(item.amount() for item in self.cart_items.all())
+        return total
+
+    def __str__(self):
+        return f"Cart ID: {self.id}"
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField()
+
+    def amount(self):
+        return self.product.amount * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.product_name} in Cart"
+    
+    
